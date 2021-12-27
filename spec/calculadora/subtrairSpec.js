6f706dcdbbac845598a18ce7c8b371ef9f0b8ba7
PedroCarvalhoAlves <pedro_alves@gec.inatel.br>
2021-12-27 13:45:22 -0300

    ADD: Implementado função de subtração.

---
+++ spec/calculadora/subtrairSpec.js
@@ -0,0 +1,23 @@
+describe("Suite de testes de subtração", function(){
+	var Calculadora = require('../../src/js/calculadora.js');
+
+	it("Deve retornar 5 para 8 e 3", function(){
+		expect(Calculadora.subtrair(8,3)).toEqual(5);
+	});
+
+	it("deve retornar 5 para 8 e 3 no formato texto", function(){
+		expect(Calculadora.subtrair('8','3')).toEqual(5);
+	});
+
+	it("deve retornar 4 para 5.5 e 1.5", function(){
+		expect(Calculadora.subtrair(5.5,1.5)).toEqual(4);
+	});
+
+	it("deve retornar 0 quando valor 1 não for numérico", function(){
+		expect(Calculadora.subtrair(undefined, 10)).toEqual(0);
+	});
+
+	it("deve retornar 0 quando valor 2 não for numérico", function(){
+		expect(Calculadora.subtrair(10, undefined)).toEqual(0);
+	});
+})
--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -7,7 +7,10 @@ var Calculadora = {
 	},
 
 	subtrair: function(num1, num2){
-		return 0;
+		num1 = parseFloat(num1);
+		num2 = parseFloat(num2);
+		if(isNaN(num1) || isNaN(num2)){return 0;}
+		return num1 - num2;
 	},
 
 	dividir: function(num1, num2){
